108e94fa6e0b3fdf35e1409237bbcac06ed5fff7
peter <peter@MacBook-Pro.local>
2020-08-26 20:33:46 +0800

    Updates

---
+++ precache-manifest.8ebf74566026c2dab9ade782e80e065d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c6c8d8675ef1a03e8a82c2e215125a",
+    "url": "/Starlink/index.html"
+  },
+  {
+    "revision": "7ca9aa8eb7408ebe21cc",
+    "url": "/Starlink/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "e1cc71c7cee4f879454b",
+    "url": "/Starlink/static/css/main.b08c5f9c.chunk.css"
+  },
+  {
+    "revision": "7ca9aa8eb7408ebe21cc",
+    "url": "/Starlink/static/js/2.3bf8dfb1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Starlink/static/js/2.3bf8dfb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1cc71c7cee4f879454b",
+    "url": "/Starlink/static/js/main.45c8715c.chunk.js"
+  },
+  {
+    "revision": "c6b721d19bbd159feae3",
+    "url": "/Starlink/static/js/runtime-main.b88209ec.js"
+  },
+  {
+    "revision": "296b75adbac5282acd4d97ead634b8b1",
+    "url": "/Starlink/static/media/satellite1.296b75ad.png"
+  },
+  {
+    "revision": "142c8c7ffbd4f05b42de3a347ada6a45",
+    "url": "/Starlink/static/media/starlink_logo.142c8c7f.svg"
+  }
+]);
